c4849a9c4fe2d9bd80d17948158672e4df099cc4
João Souza <jvsgsouza@gmail.com>
2023-11-23 10:01:53 -0300

    Ajustando DTO de login e implementando validator

---
+++ Usuario/Application/Validator/LoginValidator.cs
@@ -0,0 +1,18 @@
+﻿using Usuario.Domain.DTOs;
+using FluentValidation;
+
+namespace Usuario.Application.Validator
+{
+    public class LoginValidator : AbstractValidator<Login>
+    {
+        public LoginValidator()
+        {
+            RuleFor(login => login.Email).NotEmpty();
+            RuleFor(login => login.Email).Length(12, 255);
+            RuleFor(login => login.Email).EmailAddress();
+
+            RuleFor(login => login.Password).NotEmpty();
+            RuleFor(login => login.Password).Length(8, 30);
+        }
+    }
+}
--- Usuario/Domain/DTOs/Login.cs
+++ Usuario/Domain/DTOs/Login.cs
@@ -2,7 +2,7 @@
 {
     public class Login
     {
-        public string login { get; set; } = string.Empty;
-        public string password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
     }
 }
