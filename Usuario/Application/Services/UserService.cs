7b6441da6cf2077cb4d5cdf3e0f78ee6edfbd870
João Souza <jvsgsouza@gmail.com>
2024-02-07 20:03:33 -0300

    Tornando variável readonly

--- Usuario/Application/Services/UserService.cs
+++ Usuario/Application/Services/UserService.cs
@@ -9,7 +9,7 @@ namespace Usuario.Application.Services
 {
     public class UserService : IUserService
     {
-        private IUserRepository _userRepository;
+        private readonly IUserRepository _userRepository;
         public UserService(IUserRepository userRepository) 
         { 
             _userRepository = userRepository;
