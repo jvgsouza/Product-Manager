25cd6220aedbb309c481b864fb169719926dee8c
João Souza <jvsgsouza@gmail.com>
2024-02-08 21:04:32 -0300

    Desabilitando cobertura de código no arquivo de injeção de dependência na camada de API

--- Usuario/Application/IoC/ServiceDependencyResolver.cs
+++ Usuario/Application/IoC/ServiceDependencyResolver.cs
@@ -1,8 +1,10 @@
-﻿using Usuario.Application.Services;
+﻿using System.Diagnostics.CodeAnalysis;
+using Usuario.Application.Services;
 using Usuario.Domain.Services;
 
 namespace Usuario.Application.IoC
 {
+    [ExcludeFromCodeCoverage]
     public static class ServiceDependencyResolver
     {
         public static void AddServiceDependencyResolver(this IServiceCollection services)
