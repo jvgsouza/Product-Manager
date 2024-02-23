32824cfc2fbd17b473303b045fe49d1e17551d8a
João Souza <joao.souza@mobilus.com.br>
2024-02-22 21:05:32 -0300

    Finalizando testes de integração

---
+++ User.Test/CustomWebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Usuario.IntegrationTest
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseTestServer();
+        }
+    }
+}
